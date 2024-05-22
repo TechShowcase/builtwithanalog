8472f91ecbef1529a2d80b435b8c6f9fd0e6a362
Esther White <monacodelisa@gmail.com>
2024-05-22 23:32:52 +0300

    docs(update): minor

--- dist/.nitro/dev/index.mjs
+++ dist/.nitro/dev/index.mjs
@@ -877,7 +877,20 @@ const projects_get = defineEventHandler(() => [
   {
     "name": "AngleBrackets",
     "imageSrc": "https://raw.githubusercontent.com/TechShowcase/images/main/projects/anglebrackets.jpg",
-    "url": "https://www.angularmacedonia.org",
+    "url": "https://anglebrackets.dev",
+    "category": "Personal Website",
+    "version": "17.3.0",
+    "versionGroup": "v15 and up",
+    "analogVersion": "1.3.1",
+    "uiLib": "None",
+    "pricing": "Free",
+    "threeD": false,
+    "developer": "Duncan Faulkner"
+  },
+  {
+    "name": "Angular Macedonia",
+    "imageSrc": "https://raw.githubusercontent.com/TechShowcase/images/main/projects/angular-macedonia.jpg",
+    "url": "https://analog.anguhashblog.com",
     "category": "Community",
     "version": "17.2.3",
     "versionGroup": "v15 and up",
@@ -886,19 +899,6 @@ const projects_get = defineEventHandler(() => [
     "pricing": "Free",
     "threeD": false,
     "developer": "Angular Macedonia"
-  },
-  {
-    "name": "Angular Macedonia",
-    "imageSrc": "https://raw.githubusercontent.com/TechShowcase/images/main/projects/angular-macedonia.jpg",
-    "url": "https://analog.anguhashblog.com",
-    "category": "Open Source",
-    "version": "17.3.0",
-    "versionGroup": "v15 and up",
-    "analogVersion": "1.2.0",
-    "uiLib": "None",
-    "pricing": "Free",
-    "threeD": false,
-    "developer": "Esther White"
   },
   {
     "name": "AnguHashBlog",
--- src/server/routes/projects.get.ts
+++ src/server/routes/projects.get.ts
@@ -18,7 +18,20 @@ export default defineEventHandler(() => (
     {
       "name": "AngleBrackets",
       "imageSrc": "https://raw.githubusercontent.com/TechShowcase/images/main/projects/anglebrackets.jpg",
-      "url": "https://www.angularmacedonia.org",
+      "url": "https://anglebrackets.dev",
+      "category": "Personal Website",
+      "version": "17.3.0",
+      "versionGroup": "v15 and up",
+      "analogVersion": "1.3.1",
+      "uiLib": "None",
+      "pricing": "Free",
+      "threeD": false,
+      "developer": "Duncan Faulkner"
+    },
+    {
+      "name": "Angular Macedonia",
+      "imageSrc": "https://raw.githubusercontent.com/TechShowcase/images/main/projects/angular-macedonia.jpg",
+      "url": "https://analog.anguhashblog.com",
       "category": "Community",
       "version": "17.2.3",
       "versionGroup": "v15 and up",
@@ -27,19 +40,6 @@ export default defineEventHandler(() => (
       "pricing": "Free",
       "threeD": false,
       "developer": "Angular Macedonia"
-    },
-    {
-      "name": "Angular Macedonia",
-      "imageSrc": "https://raw.githubusercontent.com/TechShowcase/images/main/projects/angular-macedonia.jpg",
-      "url": "https://analog.anguhashblog.com",
-      "category": "Open Source",
-      "version": "17.3.0",
-      "versionGroup": "v15 and up",
-      "analogVersion": "1.2.0",
-      "uiLib": "None",
-      "pricing": "Free",
-      "threeD": false,
-      "developer": "Esther White"
     },
     {
       "name": "AnguHashBlog",
